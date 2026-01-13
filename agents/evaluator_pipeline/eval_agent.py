9d821cb19671d9d068bed8e23fba764fa2c827a6
Mohmed Husain <arkonafoob@gmail.com>
2026-01-13 15:46:36 +0530

    Remove .env from version control

--- agents/evaluator_pipeline/eval_agent.py
+++ agents/evaluator_pipeline/eval_agent.py
@@ -31,7 +31,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-flash-lite",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0.5
 )

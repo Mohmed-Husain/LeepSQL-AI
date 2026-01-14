b72505d67c3d1b048f24b996e08a09e6b61579b7
Mohmed-Husain <144891311+Mohmed-Husain@users.noreply.github.com>
2026-01-14 19:27:11 +0530

    api base url changed  in frontend

--- frontend/src/components/ConsolePage.tsx
+++ frontend/src/components/ConsolePage.tsx
@@ -10,7 +10,8 @@ import { useChatHistory } from "../contexts/ChatHistoryContext";
 
 
 
-const API_BASE_URL = "http://localhost:8000";
+// const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 interface ConsolePageProps {
   userName: string;
